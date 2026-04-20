0b78d6da586b2bc029d5d18c704fa3ea4d8f7f26
Abhinandan V Huddar <abhinandanhuddar622@gmail.com>
2026-04-20 10:48:39 +0530

    Added employee class example and explain difference between class and instance attributes

---
+++ Chapter-10_OBJECT ORIENTED PROGRAMMING/1_Class.py
@@ -0,0 +1,14 @@
+class employee:
+    sallary=10000     #class attribute
+    age=22
+
+Abhi =employee()
+Abhi.name="Abhi"      # instance attribute
+print(Abhi.name,Abhi.sallary,Abhi.age)
+
+Raju=employee()
+Raju.name="Raju"
+print(Raju.name,Raju.sallary,Raju.age)
+
+#here the name is the instance attribute and sallary and age is the class attribute 
+# that can directly belong to the class
