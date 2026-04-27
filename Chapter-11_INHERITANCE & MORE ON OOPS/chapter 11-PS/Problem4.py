8a863b7017793e33014a5b3388bde60f039f4293
Abhinandan V Huddar <abhinandanhuddar622@gmail.com>
2026-04-27 17:46:58 +0530

    implement Complex class with operator overloading for addition and multiplication

---
+++ Chapter-11_INHERITANCE & MORE ON OOPS/chapter 11-PS/Problem4.py
@@ -0,0 +1,22 @@
+# 4. Write a class ‘Complex’ to represent complex numbers, along with overloaded
+#    operators ‘+’ and ‘*’ which adds and multiplies them.
+
+class Complex:
+    def __init__(self, r,i):
+        self.i=i
+        self.r=r
+    
+    def __add__(self,c2):
+        return self.r + c2.r,self.i + c2.i
+    
+    def __mul__(self, c2):
+        real = self.r * c2.r - self.i * c2.i
+        imag = self.r * c2.i + self.i * c2.r
+        return Complex(real, imag)
+    def __str__(self):
+        return (f"self.i+self.r")
+        
+a=complex(1,2)
+b=complex(3,4)
+print(a+b)
+print(a*b)
