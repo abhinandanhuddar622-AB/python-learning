0f010fa900a027f7cd55c2b9e6f0413c6db8d356
Abhinandan V Huddar <abhinandanhuddar622@gmail.com>
2026-05-01 23:00:35 +0530

    Added examples of try-except with else and finally blocks in Python

---
+++ Chapter-12_ADVANCED PYTHON 1/9_try_else.py
@@ -0,0 +1,11 @@
+try:
+    a=int(input("Enter number: "))
+    print(a)
+
+except Exception as e:  
+    print(e)
+
+else:     # runs only if the try block executes successfully (no exception occurs)
+    print("I am inside else")
+    
+
