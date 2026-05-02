b5cdc094c14ac300a99becfd8130469c39a0a62b
Abhinandan V Huddar <abhinandanhuddar622@gmail.com>
2026-05-02 18:27:19 +0530

    use list comprehension instead of loop to generate squared list

---
+++ Chapter-12_ADVANCED PYTHON 1/14_list_comprehensions.py
@@ -0,0 +1,10 @@
+myList=[1,3,5,7,9]
+
+# squardList=[]
+# for item in myList:
+#     squardList.append(item*item)
+# print(squardList)
+
+# easy methode
+squardList=[i*i for i in myList]
+print(squardList)
