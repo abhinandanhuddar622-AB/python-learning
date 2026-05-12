5b17bbaa950f433445aac04ef5ee8b35714c7996
Abhinandan V Huddar <abhinandanhuddar622@gmail.com>
2026-05-12 14:30:05 +0530

    Added examples demonstrating string formatting using format()

---
+++ Chapter-13_ADVANCED PYTHON 2/4_format.py
@@ -0,0 +1,8 @@
+a="{0} is a good {1}".format("Abhi", "boy")      # replaces {0} with "Abhi" and {1} with "boy"
+print(a)
+
+b="{0} is sending a PDF to {1}".format("Abhi", "sam")
+print(b)
+
+b="{1} is sending a PDF to {0}".format("Abhi", "sam")
+print(b)
