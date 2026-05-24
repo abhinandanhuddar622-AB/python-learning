fa7ec5dfdc6700eddbdb9d00ab50701befda01df
Abhinandan V Huddar <abhinandanhuddar622@gmail.com>
2026-05-24 20:54:49 +0530

    Completed Python string formatting and table conversion tasks

---
+++ Chapter-13_ADVANCED PYTHON 2/chapter 13-PS/Problem2.py
@@ -0,0 +1,10 @@
+# 2. Write a program to input name, marks and phone number of a student and format it
+#    using the format function like below:
+#    “The name of the student is Abhi, his marks are 72 and phone number is 99999888”
+
+name=input("Enter Name: ")
+marks=int(input("Enter Marks: "))
+p_Number=int(input("Enter Phone Number: "))
+
+s="The name of the student is {}, his marks are {} and phone number is {}".format(name,marks,p_Number)
+print(s)
