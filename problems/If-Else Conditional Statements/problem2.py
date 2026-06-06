76c295cbea3d6eecc22dfc70fb1b3be6d1f2a72e
Abhinandan V Huddar <abhinandanhuddar622@gmail.com>
2026-06-06 22:48:05 +0530

    Add Python programs for voting eligibility and even-odd number checking

---
+++ problems/If-Else Conditional Statements/problem2.py
@@ -0,0 +1,7 @@
+# Create a program that checks if a person is eligible to vote (age >= 18).
+
+a=int(input("Enter your age: "))
+if a>=18:
+    print("your eligible to vote")
+else:
+    print("Your are not eligible to vote")
---
+++ problems/If-Else Conditional Statements/problem3.py
@@ -0,0 +1,8 @@
+# Write a program that takes a number from the user and prints “Even” if it is
+# even, otherwise “Odd”.
+
+a=int(input("Enter positive number: "))
+if a%2==0:
+    print("even Number")
+else:
+    print("odd Number")
