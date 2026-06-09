02a0d8e9581299c74fe79d338a2ab096c4ed4454
Abhinandan V Huddar <abhinandanhuddar622@gmail.com>
2026-06-09 20:40:17 +0530

    Solve while loop exercises for number printing and reverse number

---
+++ problems/While Loops/problem1.py
@@ -0,0 +1,5 @@
+# Print numbers from 1 to 10 using a while loop.
+n=1
+while n<11:
+    print(n)
+    n=n+1
---
+++ problems/While Loops/problem3.py
@@ -0,0 +1,4 @@
+# Use a while loop to reverse a given number (e.g., 123 → 321).
+
+n=321
+print(int(str(n)[::-1])) 
