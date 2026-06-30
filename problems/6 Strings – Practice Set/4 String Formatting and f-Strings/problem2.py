a7688d392b6a561819ad79d3603e7cea2ededb29
Abhinandan V Huddar <abhinandanhuddar622@gmail.com>
2026-06-30 08:48:42 +0530

    implemented string formatting examples using format() and f-strings

---
+++ problems/6 Strings – Practice Set/4 String Formatting and f-Strings/problem2.py
@@ -0,0 +1,9 @@
+# Using format() , create a sentence:
+# "My name is John and I am 25 years old."
+# by passing "John" and 25 as variables.
+
+# o the same using f-strings
+age=25
+name="John"
+
+print(f"My name is {name} and I am {age} years old.")
